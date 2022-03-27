6b6f7de33d00eacf5d36a2b7efacdf5565fbb291
amarkatkar <98218914+amarkatkar@users.noreply.github.com>
2022-03-27 21:06:44 +0530

    Create Super_Department.java

---
+++ com/greatlearning/dept/Super_Department.java
@@ -0,0 +1,21 @@
+package com.greatlearning.dept;
+
+
+public class Super_Department {
+
+    public String departmentName() {
+        return "Super Department";
+    }
+
+    public String getTodaysworks() {
+        return "no work as of now";
+    }
+
+    public String getworkDeadLine() {
+        return "Nil";
+    }
+
+    public String isTodayHoliday() {
+        return "Today is not a Holiday";
+    }
+}
