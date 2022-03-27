2839cba28c1e29feae695e2da2f81ac899b829a8
amarkatkar <98218914+amarkatkar@users.noreply.github.com>
2022-03-27 21:07:42 +0530

    Create Hr_Department.java

---
+++ com/greatlearning/dept/Hr_Department.java
@@ -0,0 +1,26 @@
+package com.greatlearning.dept;
+
+
+public class Hr_Department extends Super_Department {
+
+    public String departmentName() {
+        return "Welcome to HR Deparment ";
+    }
+
+    public String getTodayswork() {
+        return "Fill today's timesheeet and mark your attendance";
+    }
+
+    public String getworkDeadline() {
+        return "Complete by EOD";
+    }
+
+    public String getTechStackinformation() {
+        return "Today is not a holiday";
+    }
+
+    public String doActivity() {
+        return "team Lunch";
+    }
+
+}
