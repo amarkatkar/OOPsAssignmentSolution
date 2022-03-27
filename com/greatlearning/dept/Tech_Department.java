4090f1fbf7e74ef024646bc7a9aaa198de969d94
amarkatkar <98218914+amarkatkar@users.noreply.github.com>
2022-03-27 21:08:04 +0530

    Create Tech_Department.java

---
+++ com/greatlearning/dept/Tech_Department.java
@@ -0,0 +1,24 @@
+package com.greatlearning.dept;
+
+public class Tech_Department extends Super_Department {
+
+    public String departmnentName() {
+        return "Welcome to Tech Department ";
+    }
+
+    public String getTodayswork() {
+        return "Complete codining of module 1";
+    }
+
+    public String WorkDeadLine() {
+        return "Complete by EOD";
+    }
+
+    public String getTechStackinformation() {
+        return "core java";
+    }
+
+}
+
+
+
