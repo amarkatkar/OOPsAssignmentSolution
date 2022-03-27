a21e67a9efcc0919e46ac882781df514e9850b32
amarkatkar <98218914+amarkatkar@users.noreply.github.com>
2022-03-27 21:07:23 +0530

    Create Admin_Department.java

---
+++ com/greatlearning/dept/Admin_Department.java
@@ -0,0 +1,17 @@
+package com.greatlearning.dept;
+
+public class Admin_Department extends Super_Department {
+
+    public String departmentName() {
+        return "Welcome to admin department";
+    }
+
+    public String getTodayswork() {
+        return "Complete your documents Submission";
+    }
+
+    public String getworkDeadLine() {
+        return "Complete by EOD";
+    }
+
+}
